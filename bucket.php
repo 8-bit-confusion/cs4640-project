fd3aa53bc3c18c5408a86f6e2ad19e22c6e6f542
Lilli <hrncirlilli@gmail.com>
2025-10-28 18:53:33 -0400

    Set up bucket controller

--- bucket.php
+++ bucket.php
@@ -1,14 +1,27 @@
 <?php 
 // Created this so my messy code doesn't get in the way of other
 // development! Feel free to integrate later. 
+require 'vendor/autoload.php';
 use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+class BucketController {
+    private $client;
+    private $bucket;
+    public function __construct() {
+        $this->bucket = config::$bucket["bucket"];
 
-$client = new S3Client(['region' => 'us-east-2']);
-$results = $client->listBuckets();
-var_dump($results);
+        $region = config::$client["region"];
+        $access_key = config::$client["aws_access_key_id"];
+        $secret_key = config::$client["aws_secret_access_key"];
+        $this->client = new S3Client(['region' => $region,
+            'version' => 'latest',
+            'credentials' => [
+                'key' => $access_key,
+                'secret' => $secret_key
+            ]
+        ]);
+    }
 
-$this->bucketName = "f25-cs4640"
 
-
-
+}
 ?>
