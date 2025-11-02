0d6931f0e7ac5a5ef4d7aaf318dbd7025771da14
8-bit-confusion <lilywasko@gmail.com>
2025-11-02 12:30:16 -0500

    deleting comments

--- views/resource.php
+++ views/resource.php
@@ -116,7 +116,13 @@
                                     </form>
                                 <?php } ?>
                                 <?php if ($comment["author"] == $_SESSION["username"] || $resource_data["author"] == $_SESSION["username"]) { ?>
-                                    <button class="link-button">Delete comment</button>
+                                    <form method="post">
+                                        <input type="hidden" name="command" value="do-delete-comment">
+                                        <input type="hidden" name="target_comment" value="<?php echo $comment["id"]; ?>">
+                                        <input type="hidden" name="comment_author" value="<?php echo $comment["author"]; ?>">
+                                        <input type="hidden" name="target_resource" value="<?php echo $resource_data["id"]; ?>">
+                                        <button class="link-button">Delete comment</button>
+                                    </form>
                                 <?php } ?>
                             </div>
                             <?php foreach ($comments as $child_comment) { ?>
@@ -131,7 +137,13 @@
                                         </div>
                                         <div class="flex-row">
                                             <?php if ($child_comment["author"] == $_SESSION["username"] || $resource_data["author"] == $_SESSION["username"]) { ?>
-                                                <button class="link-button">Delete comment</button>
+                                                <form method="post">
+                                                    <input type="hidden" name="command" value="do-delete-comment">
+                                                    <input type="hidden" name="target_comment" value="<?php echo $child_comment["id"]; ?>">
+                                                    <input type="hidden" name="comment_author" value="<?php echo $child_comment["author"]; ?>">
+                                                    <input type="hidden" name="target_resource" value="<?php echo $resource_data["id"]; ?>">
+                                                    <button class="link-button">Delete comment</button>
+                                                </form>
                                             <?php } ?>
                                         </div>
                                     </div>
