f065bb0f121867414a1d07de40e747c3f26a8886
8-bit-confusion <lilywasko@gmail.com>
2025-11-01 20:37:04 -0400

    changed file upload and tag styling for usability

--- views/create.php
+++ views/create.php
@@ -36,24 +36,8 @@ Deployed URL: https://cs4640.cs.virginia.edu/gzg8pf/project-static/
                         <label for="resource-description">Description</label><br>
                         <textarea class="register-input" style="resize:none;" name="description" id="resource-description" rows="4"></textarea>
                     </div>
-                    <div class="file-container">
-                        <div class="styled-file">
-                            <img style="height:20px;justify-self:left;" src="styles/attach-file-icon.png" alt="File successfully attached.">
-                            <span style="flex-grow: 1;font-size: small;">cover_image.png</span>
-                            <img class="icon-button" style="height:20px;justify-self:left;" alt="This image will be the cover picture." src="styles/picture-icon.svg">
-                            <img class="icon-button" style="height:20px;justify-self:left;" alt="Remove this file from resource." src="styles/close-icon.svg">
-                        </div>
-                        <div style="display:flex;flex-direction:row-reverse;">
-                            <label for="resource-files" class="custom-file-upload">Upload Files</label><br>
-                            <input class="register-input" type="file" name="files[]" id="resource-files" multiple>
-                        </div>
-                    </div>
-                    <div class="tag-container">
-                        <div class="flex-row tag">
-                            <textarea title="tags" name="tags" id="resource-tags"></textarea>
-                            <img src="styles/close-icon.svg" alt="Delete this tag.">
-                        </div>
-                    </div>
+                    <input class="register-input" type="file" name="files[]" id="resource-files" multiple>
+                    <textarea class="register-input comment-input" rows="2" title="tags" name="tags" id="resource-tags" placeholder="Enter tags as space separated strings with only letters and underscores; e.g. 'tag_a tag_b'"></textarea>
                     <div class="div-input submit">
                         <button class="styled-button" type="submit" name="publish" id="publish-resource">Publish Resource</button>
                     </div>
